ac69b61ca332028eb652c70ad8a2a1cf0a449cf1
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2021-09-29 20:34:06 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -4,15 +4,12 @@ function login(e){
     let form = document.getElementById('form');
 
     let retrieveData = JSON.parse(localStorage.getItem("PulseUsers"));
-    //console.log(retrieveData);
 
     let email = form.email.value;
     let password = form.password.value;
 
     let flag = false;
     retrieveData.forEach(function(i) {
-      //  console.log(i);
-      //console.log(i.email,i.password+" user"+email, password);
         if(email == i.email && password == i.password){
             flag = true;
         }
