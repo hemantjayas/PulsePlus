23cc66be7f5481ca22350e4b7ff87bae747e9678
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2021-10-03 11:33:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,3 +48,34 @@ function startSlideshow() {
 
 }
 startSlideshow();
+
+startSlideshow();
+
+let images2 = [
+    "https://res.cloudinary.com/pulse-pharmacy/image/upload/product/novy-pain-oil-pc-41766",
+    "https://res.cloudinary.com/pulse-pharmacy/image/upload/product/conarcs-diabetic-ointment-pc-41767",
+    "https://res.cloudinary.com/pulse-pharmacy/image/upload/product/shigaa-anti-lice---dandruff-oil-pc-44072",
+    "https://res.cloudinary.com/pulse-pharmacy/image/upload/product/pankajakasthuri-orthoherb-tablet-30-s-pc-26998",
+]
+
+let container2 = document.getElementById("didYouTryBox");
+function startSlideshow1() {
+
+    let counter = 0;
+
+   let interval =  setInterval(function(){
+
+        container2.innerHTML = null;
+
+        if(counter=== images2.length){
+            counter = 0;
+        }
+        let img = document.createElement("img");
+        img.src = images2[counter];
+        container2.append(img);
+        counter++;
+    },3000);
+
+
+}
+startSlideshow1();
