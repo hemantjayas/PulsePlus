77b96d5164a3b1978f4fe1ccadfe230319904373
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2021-10-03 11:34:59 +0530

    Update product.js

--- product.js
+++ product.js
@@ -24,32 +24,32 @@ var data = [
 
     {
         name: 'Disprin Tablet',
-        price: '8.4',
+        price: '8',
         img: 'https://res.cloudinary.com/pulse-pharmacy/image/upload/product/disprin-tablet-pc-8031',
 
 
     },
     {
         name: 'Monocef Injection 1 gm',
-        price: '48.8',
+        price: '48',
         img: 'https://res.cloudinary.com/pulse-pharmacy/image/upload/product/monocef-injection-1-gm-pc-21243',
     },
     {
         name: 'Crocin Pain Relief Tablet',
-        price: '48.75',
+        price: '48',
         img: 'https://res.cloudinary.com/pulse-pharmacy/image/upload/product/crocin-pain-relief-tablet-pc-8309',
 
     },
     {
         name: 'Dettol Liquid 110 ml',
-        price: '58.68',
+        price: '58',
         img: 'https://res.cloudinary.com/pulse-pharmacy/image/upload/product/dettol-liquid-110-ml-pc-7924',
 
 
     },
     {
         name: 'Paracetamol Injection 30 ml',
-        price: '15.3',
+        price: '15',
         img: 'https://www.pulseplus.in/content/images/form/form-surg-1.png',
 
 
